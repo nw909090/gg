6cb2ffbe265d122f62853a792eb0a1ae29c6153f
nw909090 <nw9090909090990@gmail.com>
2025-04-13 09:42:59 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID || "session id",
+SESSION_ID: process.env.SESSION_ID || "LAKA-MD=0sJGGApY#X89nU5KgwJEYaldtlJ196HvLEFN5_-inWuR2ZbYkdRI",
 PREFIX: process.env.PREFIX || '.' ,
 POSTGRESQL_URL: process.env.POSTGRESQL_URL === undefined ? 'postgresql://postgres:lakamd@db.ndduyrkebubwlbwiljee.supabase.co:5432/postgres' : process.env.POSTGRESQL_URL
 };
